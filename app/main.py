afe628e965036a535f5ba11cfb02785e86af13fc
Ermek Abdiyev <abdiyevermek6@gmail.com>
2025-09-19 05:19:09 +0500

    в импорте отрефакторил имя файла database.py в db.py,так как database не существовало

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routes import tasks
-from app.database import engine
+from app.db import engine
 from app.models import Base
 
 
